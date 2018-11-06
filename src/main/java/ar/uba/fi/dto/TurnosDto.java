53ba7ccff024feca63cf801a5cd1dc27f4f05979
lpennese <lpennese@NL-LPENNESE.cx.ar>
2018-11-06 16:43:49 -0300

    dto turnos

---
+++ src/main/java/ar/uba/fi/dto/TurnosDto.java
@@ -0,0 +1,49 @@
+package ar.uba.fi.dto;
+
+import java.util.Date;
+
+public class TurnosDto {
+	
+	private Integer id;
+	private Date fecha;
+	private String especialidad;
+	private String estado;
+	
+	
+	public TurnosDto(Integer id,Date fecha, String especialidad, String estado) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.especialidad = especialidad;
+		this.estado = estado;
+	}
+	
+	
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getEspecialidad() {
+		return especialidad;
+	}
+	public void setEspecialidad(String espacialidad) {
+		this.especialidad = espacialidad;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
