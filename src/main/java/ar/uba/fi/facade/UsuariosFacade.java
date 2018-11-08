36677f3809871520f34372841e0f23e9f5a8839b
pellarolojuani <pellarolojuani@gmail.com>
2018-11-08 17:03:31 -0300

    backend turnos. tiempo: 0.5 hora

--- src/main/java/ar/uba/fi/facade/UsuariosFacade.java
+++ src/main/java/ar/uba/fi/facade/UsuariosFacade.java
@@ -1,11 +1,12 @@
 package ar.uba.fi.facade;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ar.uba.fi.dto.UsuarioDto;
 import ar.uba.fi.mongo.facade.MongoDBUsuarioFacade;
-
 
 @Service
 public class UsuariosFacade {
@@ -15,6 +16,24 @@ public class UsuariosFacade {
 	public void crearUsuario(UsuarioDto usuario) {
 		mongoDBCUsersFacade.crearUsuario(usuario);
 	}
-	
-	
+
+	public void editarUsuario(UsuarioDto usuario) {
+		mongoDBCUsersFacade.editarUsuario(usuario);
+	}
+
+	public void removerUsuario(UsuarioDto usuario) {
+		mongoDBCUsersFacade.removerUsuario(usuario);
+	}
+
+	public List<UsuarioDto> getAllUsuarios() {
+		return mongoDBCUsersFacade.getAllUsuarios();
+	}
+
+	public UsuarioDto getUsuarioById(String id) {
+		return mongoDBCUsersFacade.getUsuarioById(id);
+	}
+
+	public UsuarioDto getUsuarioByNombreUsuario(String nombreUsuario) {
+		return mongoDBCUsersFacade.getUsuarioByNombreUsuario(nombreUsuario);
+	}
 }
--- src/main/java/ar/uba/fi/mongo/repository/TurnoRepository.java
+++ src/main/java/ar/uba/fi/mongo/repository/TurnoRepository.java
@@ -1,9 +1,20 @@
 package ar.uba.fi.mongo.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import ar.uba.fi.dto.TurnosDto;
 
 public interface TurnoRepository  extends MongoRepository<TurnosDto, String>{
+
+	public List<TurnosDto> findByFecha(Date fecha);
+
+	public List<TurnosDto> findByEspecialidad(String especialidad);
+	
+	public List<TurnosDto> findByEstado(String estado);
+	
+	public List<TurnosDto> findByMedico(String medico);
 
 }
